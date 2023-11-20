ccbbb229a10bfb9695e4d01511f3bdc02d1911b0
Arthur Brook <arthurbrook1@gmail.com>
2023-11-20 19:02:30 +0200

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -15,9 +15,9 @@ imageContainer.insertAdjacentHTML('beforeend', cardsMarkup);
 function createImageCardsMarkup(Items) {
   return Items.map(({ preview, original, description }) => {
     return `<li class="gallery__item">
-   <a class="gallery__link" href="${original}">
+    <a class="gallery__link" href="${original}">
       <img class="gallery__image" src="${preview}" alt="${description}" />
-   </a>
+    </a>
 </li>`;
   }).join('');
 }
